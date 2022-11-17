adc980302d8b5d60ea32f9bf79000db79391525d
Beni <69584313+benilol98@users.noreply.github.com>
2022-11-17 10:28:53 +0100

    dsdasdasdasdas elektronika ora xd

---
+++ binary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int len = 0;
+    
+    for (int i = 0; num != 0; i++){
+        num =  num/2;
+    }
+    
+    int num = 100;
+    
+    while (num != 0){
+        printf("%d", num%2);
+        num = num/2;
+    }
+}
+// work in progress
