dbe4d563cc174c40c195466b56f80de82c5b0cba
Beni <69584313+benilol98@users.noreply.github.com>
2022-11-17 15:26:46 +0100

    done
    also added shitty comments

--- binary.c
+++ binary.c
@@ -2,22 +2,31 @@
 
 int main()
 {
+    // szam
     int num = 10;
+    // ugyanaz a szam de ezzel nezem meg h milyen hosszu legyen az array
     int testnum = num;
+    // ebbe szamolom h milyen hosszu legyen a hossusagot
     int len = 0;
     
-    for (int i = 0; testnum != 0; i++){
-        testnum =  testnum/2;
+    // while loop ami megnezi h milyen hosszu lesz az array
+    while (testnum != 0){
+        testnum = testnum/2;
         len++;
     }
     
-    
-    
     printf("array len: %d\n", len);
+    // letrehozom az arrayt len hosszsaggal
+    int array[len];
     
-    while (num != 0){
-        printf("%d", num%2);
+    // for loop ami atkonvertalja binarisba es berakja egy arraybe a maradekokat
+    for (int i = 0; num != 0; i++){
+        array[i] = num%2;
         num = num/2;
     }
+    
+    // for loop az array visszafele printelesehez
+    for (int i = len-1; i >= 0; i--){
+        printf("%d", array[i]);
+    }
 }
-// wip
